34f9e54976d907e87e725b30d97642428589e4ce
sreerajsree <sreeandsreeraj@gmail.com>
2022-11-22 19:33:02 +0530

    margin on navbar

--- components/Generic/EmailLink.js
+++ components/Generic/EmailLink.js
@@ -3,7 +3,7 @@ export default function EmailLink({ className }) {
     <div className="inline-flex">
       <a className={className} href="/">
         <h2 className="text-xl md:text-2xl">Taru Goswami </h2>
-        <img src="/assets/butterfly.svg" className="h-7 w-7 ml-2 mr-3 animate-pulse" alt="logo" />
+        <img src="/assets/butterfly.svg" className="h-7 w-7 ml-2 mr-6 animate-pulse" alt="logo" />
       </a>
     </div>
   );
